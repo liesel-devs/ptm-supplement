7ce2de239fab455a5798be7045cb5aa165c973bf
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2026-01-17 11:24:11 +0100

    update fh-gaussian code

--- application-fh/007-gaussian-nonlinear-nori/run.py
+++ application-fh/007-gaussian-nonlinear-nori/run.py
@@ -1,14 +1,14 @@
-from pathlib import Path
-import pandas as pd
-import jax.numpy as jnp
+import argparse
 import time
 from datetime import datetime
-import argparse
+from pathlib import Path
 
+import jax
+import jax.numpy as jnp
 import liesel.goose as gs
 import liesel_ptm as ptm
-import jax
-from liesel_ptm import ps, term, lin
+import pandas as pd
+from liesel_ptm import lin, ps, term
 
 jax.config.update("jax_enable_x64", True)
 
@@ -18,7 +18,7 @@ parser.add_argument(
     "--jobdir", type=str, default="application-fh/007-gaussian-nonlinear-nori"
 )
 parser.add_argument("--jobrow", type=int, default=0)
-parser.add_argument("--testing", type=bool, default=True)
+parser.add_argument("--testing", type=int, default=1)
 args, _ = parser.parse_known_args()
 
 
@@ -170,6 +170,38 @@ crps = meval.crps_sample(
     subsamples_n=min(1000, POSTERIOR),
 )
 
+probs = jnp.linspace(0.005, 0.995, 25)
+
+crps_by_qs = meval.crps(
+    probs=probs,
+    newdata=newdata | {"response": test["cholst"].to_numpy()},
+    k=10,
+).mean()
+
+key, subkey = jax.random.split(key)
+crps_by_qs_estimated = meval.crps_by_estimated_quantiles(
+    key=subkey,
+    probs=probs,
+    newdata=newdata | {"response": test["cholst"].to_numpy()},
+    m=3,
+).mean()
+
+quantile_crps_by_qs = meval.crps(
+    probs=probs,
+    newdata=newdata | {"response": test["cholst"].to_numpy()},
+    k=10,
+    quantile_weight_fn=lambda alpha: (2 * alpha - 1) ** 2,
+).mean()
+
+key, subkey = jax.random.split(key)
+quantile_crps_by_qs_estimated = meval.crps_by_estimated_quantiles(
+    key=subkey,
+    probs=probs,
+    newdata=newdata | {"response": test["cholst"].to_numpy()},
+    quantile_weight_fn=lambda alpha: (2 * alpha - 1) ** 2,
+    m=3,
+).mean()
+
 # ..............................................................................
 # ---- WAIC ----
 # ..............................................................................
@@ -185,6 +217,10 @@ dist_summary = pd.DataFrame(
     {
         "waic": waic,
         "crps": crps,
+        "crps_by_qs": crps_by_qs,
+        "quantile_crps_by_qs": quantile_crps_by_qs,
+        "crps_by_qs_estimated": crps_by_qs_estimated,
+        "quantile_crps_by_qs_estimated": quantile_crps_by_qs_estimated,
     },
     index=[0],  # type: ignore
 )
